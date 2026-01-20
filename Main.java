6aa154720a087654c04ee71a6ad7de6c1f158c74
Ombraze <yanis.dahmane06@gmail.com>
2026-01-20 10:34:50 +0100

    menu principal

---
+++ Main.java
@@ -0,0 +1,8 @@
+import Menu.Principal_Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal_Menu menu = new Principal_Menu();
+        menu.afficherMenu();
+    }
+}
